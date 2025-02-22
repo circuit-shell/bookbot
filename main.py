665bd08753dcc30412b566576acef5136ddf5240
Armando del Río <adrio.6180@gmail.com>
2025-02-21 20:46:54 -0600

    feat: do

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-from stats import get_chars_dict, get_num_words
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
 
 
 def main():
@@ -6,8 +10,8 @@ def main():
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
-    print(f"{num_words} words found in the document")
-    print(chars_dict)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
 
 
 def get_book_text(path):
@@ -15,4 +19,19 @@ def get_book_text(path):
         return f.read()
 
 
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+
 main()
+
